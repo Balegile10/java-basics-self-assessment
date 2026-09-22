c8172a0be4ecd61e47fcf134ab81c8644492054f
Balegile10 <kbalegile@gmail.com>
2026-09-22 14:34:42 +0200

    Q5 ExpresssionEvaluation Solution

---
+++ Q5_ExpressionEvaluation/src/q5_expressionevaluation/Q5_ExpressionEvaluation.java
@@ -0,0 +1,23 @@
+
+package q5_expressionevaluation;
+
+
+public class Q5_ExpressionEvaluation {
+
+    
+    public static void main(String[] args) {
+        // Given values
+        double a = 5;
+        double b = 3;
+        double c = 4;
+        double d = 2;
+        double e = 6;
+        double f = 1.5;
+
+        // Compute (a + b) / c + (d * e) - f 
+        double result = (a + b) / c + (d * e) - f;
+
+        System.out.println("Result: " + result);
+    }
+    
+}
