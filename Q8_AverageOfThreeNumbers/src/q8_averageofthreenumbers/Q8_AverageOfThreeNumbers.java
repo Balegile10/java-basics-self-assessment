b626332b79150ccb42c249a6e89097685ba0be50
Balegile10 <kbalegile@gmail.com>
2026-09-22 14:53:15 +0200

    Q8 AverageOfThreeNumbers Solution

---
+++ Q8_AverageOfThreeNumbers/src/q8_averageofthreenumbers/Q8_AverageOfThreeNumbers.java
@@ -0,0 +1,20 @@
+
+package q8_averageofthreenumbers;
+
+
+public class Q8_AverageOfThreeNumbers {
+
+    
+    public static void main(String[] args) {
+        // Given values
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+
+        // Calculate average
+        int average = (num1 + num2 + num3) / 3;
+
+        System.out.println("Average of three numbers is: " + average);
+    }
+    
+}
