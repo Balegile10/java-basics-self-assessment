0dfb99a13482a6eff8c8b5e7fb5281fce5c9b003
Balegile10 <kbalegile@gmail.com>
2026-09-22 14:14:48 +0200

    Q2_BasicArithmaticOperations Solution

---
+++ Q2_BasicArithmaticOperations/src/q2_basicarithmaticoperations/Q2_BasicArithmaticOperations.java
@@ -0,0 +1,20 @@
+
+package q2_basicarithmaticoperations;
+
+
+public class Q2_BasicArithmaticOperations {
+
+    public static void main(String[] args) {
+        // Given values
+        int a = 125;
+        int b = 24;
+
+        // Perform and print arithmetic operations
+        System.out.println(a + " + " + b + " = " + (a + b));
+        System.out.println(a + " - " + b + " = " + (a - b));
+        System.out.println(a + " x " + b + " = " + (a * b));
+        System.out.println(a + " / " + b + " = " + (a / b));
+        System.out.println(a + " mod " + b + " = " + (a % b));
+    }
+    
+}
