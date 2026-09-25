fd3572517e8babd9b21eaec3f75b5b8cc84b8752
Balegile10 <kbalegile@gmail.com>
2026-09-25 14:28:01 +0200

    Q20-HexaToDeciConverter Solution

---
+++ Q20-HexaToDeciConverter/src/q20/hexatodeciconverter/Q20HexaToDeciConverter.java
@@ -0,0 +1,12 @@
+
+package q20.hexatodeciconverter;
+
+public class Q20HexaToDeciConverter {
+
+    public static void main(String[] args) {
+        String hex = "25";
+        int decimal = Integer.parseInt(hex, 16);
+        System.out.println("Equivalent decimal number is: " + decimal);
+    }
+    
+}
