5c3c44b16ff870487efc6afecba25128faccd8fe
Balegile10 <kbalegile@gmail.com>
2026-09-25 14:18:17 +0200

    Q15-DecimalToBBinaryConverter Solution

---
+++ Q15-DecimalToBinaryConverter/src/q15/decimaltobinaryconverter/Q15DecimalToBinaryConverter.java
@@ -0,0 +1,11 @@
+
+package q15.decimaltobinaryconverter;
+
+public class Q15DecimalToBinaryConverter {
+
+    public static void main(String[] args) {
+       int number = 5;
+        System.out.println("Binary number is: " + Integer.toBinaryString(number));
+    }
+    
+}
