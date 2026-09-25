5ff720bf6f6d8247776172953018ab0daed14f1c
Balegile10 <kbalegile@gmail.com>
2026-09-25 13:36:17 +0200

    Q11 SwapVariables Solution

---
+++ Q11-SwapVariables/src/q11/swapvariables/Q11SwapVariables.java
@@ -0,0 +1,20 @@
+
+package q11.swapvariables;
+
+public class Q11SwapVariables {
+
+    
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 27;
+
+        System.out.println("Before swapping : a, b = " + a + ", " + b);
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After swapping : a, b = " + a + ", " + b);
+    }
+    
+}
