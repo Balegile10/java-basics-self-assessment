1d0d57f11ab1c6e4dbe129e012db4fa9c65add80
Balegile10 <kbalegile@gmail.com>
2026-09-25 14:20:21 +0200

    Q16-DeciToHexaConverter Solution

---
+++ Q16-DeciToHexConverter/src/q16/decitohexconverter/Q16DeciToHexConverter.java
@@ -0,0 +1,11 @@
+
+package q16.decitohexconverter;
+
+public class Q16DeciToHexConverter {
+
+    public static void main(String[] args) {
+        int number = 15;
+        System.out.println("Hexadecimal number is : " + Integer.toHexString(number).toUpperCase());
+    }
+    
+}
