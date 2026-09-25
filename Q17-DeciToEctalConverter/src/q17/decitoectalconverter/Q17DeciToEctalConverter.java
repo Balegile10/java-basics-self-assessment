e333a0ac2b98230e3005ac8934985e734729af4a
Balegile10 <kbalegile@gmail.com>
2026-09-25 14:22:25 +0200

    Q17-DeciToOctalConverter Solution

---
+++ Q17-DeciToEctalConverter/src/q17/decitoectalconverter/Q17DeciToEctalConverter.java
@@ -0,0 +1,11 @@
+
+package q17.decitoectalconverter;
+
+public class Q17DeciToEctalConverter {
+
+    public static void main(String[] args) {
+       int number = 15;
+        System.out.println("Octal number is: " + Integer.toOctalString(number));
+    }
+    
+}
